c36f03d61eae6ca84d5b537c071319cfa634e3a8
Meenakshi <singh99meenakshi@gmail.com>
2024-03-08 21:54:10 +0530

    Jsx work

---
+++ src/File1.mjs
@@ -0,0 +1,4 @@
+import ui , {a,b}from './File2.mjs'
+console.log(ui);
+console.log(a);
+console.log(b);
---
+++ src/File2.mjs
@@ -0,0 +1,6 @@
+const a = "Meenakshi";
+const b = "Rani";
+const c = "Meenu";
+export default c;
+export {a};
+export {b};
